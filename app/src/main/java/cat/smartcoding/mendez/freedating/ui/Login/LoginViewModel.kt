2ac8a45fe6b97a1c000c5b2c7d99d85e92465b7d
wasach <74709691+wasach@users.noreply.github.com>
2021-12-01 20:43:52 +0100

    create: - login_fragment - register_fragment
    Se añade login/register a navigation y se marca login como principal.

---
+++ app/src/main/java/cat/smartcoding/mendez/freedating/ui/Login/LoginViewModel.kt
@@ -0,0 +1,7 @@
+package cat.smartcoding.mendez.freedating.ui.Login
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/cat/smartcoding/mendez/freedating/ui/register/RegisterViewModel.kt
@@ -0,0 +1,7 @@
+package cat.smartcoding.mendez.freedating.ui.register
+
+import androidx.lifecycle.ViewModel
+
+class RegisterViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
