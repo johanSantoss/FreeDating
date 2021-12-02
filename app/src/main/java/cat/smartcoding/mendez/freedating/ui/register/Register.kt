30836af2096060c632b14f98d1cc8a5c2e82c197
wasach <74709691+wasach@users.noreply.github.com>
2021-12-02 17:28:28 +0100

    register: - process disseny
    Add color to values

--- app/src/main/java/cat/smartcoding/mendez/freedating/ui/register/Register.kt
+++ app/src/main/java/cat/smartcoding/mendez/freedating/ui/register/Register.kt
@@ -20,7 +20,7 @@ class Register : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.register_fragment, container, false)
+        return inflater.inflate(R.layout.fragment_register, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
