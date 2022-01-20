bd253f2ada65b553808fbbefd689fe898077a237
wasach <74709691+wasach@users.noreply.github.com>
2022-01-20 02:43:33 +0100

    ++ succes scrolling fragment "userFragment/RegistreFragment" y corrección de los margenes
    - modificar registre_fragment
      -- cambiar link del storage
      -- preguntar si hay que inicializar las variables de la "DataClass"
    
    - modificar user_fragment
      -- generar slider de imagenes
         -- abrir imagenes del storage
      -- crear metodo "cargarDatos()" -> ubicado en el onCreate del fragment y ha de cargar los datos de la RealTimeDataBase del usuario y setearlos en los campos

--- app/src/main/java/cat/smartcoding/mendez/freedating/ui/register/Register.kt
+++ app/src/main/java/cat/smartcoding/mendez/freedating/ui/register/Register.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.get
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -15,11 +16,15 @@ import cat.smartcoding.mendez.freedating.MainActivity
 import cat.smartcoding.mendez.freedating.R
 import cat.smartcoding.mendez.freedating.databinding.FragmentRegisterBinding
 import cat.smartcoding.mendez.freedating.ui.Login.LoginDirections
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
 
 class Register : Fragment() {
 
     private lateinit var binding: FragmentRegisterBinding
     private lateinit var viewModel: RegisterViewModel
+    private lateinit var database: FirebaseDatabase
 
     companion object {
         fun newInstance() = Register()
@@ -43,22 +48,28 @@ class Register : Fragment() {
         // Get the viewModel
         viewModel = ViewModelProvider(this).get(RegisterViewModel::class.java)
 
+        // Conection to Storage
+        // cambiar el link del Storage por el correcto, este no es valido --------------------------------------------------------------------------------------------------------------------
+        database = FirebaseDatabase.getInstance("https://projecte1-6bfbd-default-rtdb.europe-west1.firebasedatabase.app/")
 
         binding.btnNewRegister.setOnClickListener {
-
-            // hay que generar un control para que los campos no esten vacios antes de iniciar este proceso--------------------------------------------------------------------------------------------------
-            subirDatos()
-            createAccount(viewModel.email.value.toString(), viewModel.password.value.toString(), viewModel.password2.value.toString())
-
+            if (binding.editTextName.text == null ||
+                binding.editTextEdat.text == null ||
+                binding.radioGroupRegistre.checkedRadioButtonId == null ||
+                binding.editTextCiutat.text == null ||
+                binding.editTextEmailRegister.text == null ||
+                binding.editTextPassword.text == null ||
+                binding.editTextPassword2.text == null) {
+                Toast.makeText(activity, "Faltan datos por completar!", Toast.LENGTH_SHORT).show()
+            } else {
+                saveDatesUserViewModel()
+                createAccount(viewModel.email.value.toString(), viewModel.password.value.toString(), viewModel.password2.value.toString())
+            }
         }
 
         binding.btnClear.setOnClickListener {
-            binding.editTextEmailRegister.text.clear()
-            binding.editTextPassword.text.clear()
-            binding.editTextPassword2.text.clear()
+            clearDates()
         }
-
-
 
         return  binding.root
 //        return inflater.inflate(R.layout.fragment_register, container, false)
@@ -76,6 +87,7 @@ class Register : Fragment() {
         super.onStop()
         val supportActionBar = (requireActivity() as AppCompatActivity).supportActionBar
         (activity as MainActivity).enableMenus()
+        if (viewModel.registro.value == 1) viewModel.resetValues()
         supportActionBar?.show()
     }
 
@@ -96,7 +108,11 @@ class Register : Fragment() {
                         Log.d(TAG, "createUserWithEmail:success")
 //                        val user = (activity as MainActivity).getAuth().currentUser
                         Toast.makeText(activity, "Register success!", Toast.LENGTH_SHORT).show()
-                        subirDatos()
+                        // guardar los datos en la DDBB
+                        saveDatesUserDataBase()
+                        // modificar el estado del registro a 1 para que se haga reset
+                        viewModel.setEstadoRegistro(1)
+                        // go to login fragment
                         val action = RegisterDirections.actionRegisterToLogin()
                         NavHostFragment.findNavController(this).navigate(action)
                     } else {
@@ -111,13 +127,58 @@ class Register : Fragment() {
         }
     }
 
-    private fun subirDatos(){
-        // guardar datos en el viewModel--------------------------------------------------------------------------------------------------------------------------------------
-        viewModel.setEmail(binding.editTextEmailRegister.text.toString().trim())
-        viewModel.setPassword(binding.editTextPassword.text.toString().trim())
-        viewModel.setPassword2(binding.editTextPassword2.text.toString().trim())
-
-        // guardar datos en "RealTimeDataBase" ---------------------------------------------------------------------------------------------------------------------------------
-
+    private fun clearDates(){
+        binding.editTextName.text.clear()
+        binding.editTextEdat.text.clear()
+        binding.radioGroupRegistre.clearCheck()
+        binding.editTextCiutat.text.clear()
+        binding.editTextEmailRegister.text.clear()
+        binding.editTextPassword.text.clear()
+        binding.editTextPassword2.text.clear()
     }
+
+    data class DatosUsuari(
+        // preguntar si hay que inicializar las variables------------------------------------------------------------------------------------------------------------------------------
+        var nom: String,
+        var edat: String,
+        var sexe: String,
+        var ciutat: String,
+        var email: String,
+    )
+    private fun saveDatesUserViewModel(){
+        if (binding.editTextName.text != null){
+            viewModel.setNom(binding.editTextName.text.toString().trim())
+        }
+        if (binding.editTextEdat.text != null){
+            viewModel.setEdatUser(binding.editTextEdat.text.toString().trim())
+        }
+        if (binding.radioGroupRegistre.checkedRadioButtonId != null){
+            viewModel.setSexeUser(binding.radioGroupRegistre.checkedRadioButtonId)
+        }
+        if (binding.editTextCiutat.text != null){
+            viewModel.setCiutatUser(binding.editTextCiutat.text.toString().trim())
+        }
+        if (binding.editTextEmailRegister.text != null){
+            viewModel.setEmail(binding.editTextEmailRegister.text.toString().trim())
+        }
+        if (binding.editTextPassword.text != null){
+            viewModel.setPassword(binding.editTextPassword.text.toString().trim())
+        }
+        if (binding.editTextPassword2.text != null){
+            viewModel.setPassword2(binding.editTextPassword2.text.toString().trim())
+        }
+    }
+    private fun saveDatesUserDataBase(){
+        val auth = Firebase.auth
+        var sexe : String
+        // se setea el sexe
+        if (viewModel.sexe.value == binding.radioBtn1.id) sexe = "Dona" else sexe = "Home"
+        // se genera una clase de USER con todos los datos del usuario
+        val user = DatosUsuari(viewModel.nom.value.toString(), viewModel.edat.value.toString(),  sexe, viewModel.ciutat.value.toString(), auth.currentUser?.email.toString())
+        // Se genera el acceso a la DDBB
+        val myRef = database.getReference("${auth.currentUser?.uid}/userDates")
+        // Se settean y suben los datos del nuevo usuario
+        myRef.setValue(user)
+    }
+
 }
--- app/src/main/java/cat/smartcoding/mendez/freedating/ui/register/RegisterViewModel.kt
+++ app/src/main/java/cat/smartcoding/mendez/freedating/ui/register/RegisterViewModel.kt
@@ -9,6 +9,46 @@ import cat.smartcoding.mendez.freedating.ui.Login.LoginViewModel
 
 class RegisterViewModel : ViewModel() {
 
+    // nom
+    private val _nom = MutableLiveData<String>("")
+    val nom: LiveData<String> get() = _nom
+    fun setNom (userName : String){
+        _nom.value = userName
+    }
+    private fun resetNom(){
+        _nom.value = ""
+    }
+
+    // edat
+    private val _edat = MutableLiveData<String>("")
+    val edat: LiveData<String> get() = _edat
+    fun setEdatUser (edatUser : String){
+        _edat.value = edatUser
+    }
+    private fun resetEdat(){
+        _edat.value = ""
+    }
+
+    // sexe
+    private val _sexe = MutableLiveData<Int?>(null)
+    val sexe: LiveData<Int?> get() = _sexe
+    fun setSexeUser (sexeUser : Int){
+        _sexe.value = sexeUser
+    }
+    private fun resetSexe(){
+        _sexe.value = null
+    }
+
+    // ciutat
+    private val _ciutat = MutableLiveData<String>("")
+    val ciutat: LiveData<String> get() = _ciutat
+    fun setCiutatUser (ciutatUser : String){
+        _ciutat.value = ciutatUser
+    }
+    private fun resetCiutat(){
+        _ciutat.value = ""
+    }
+
 
     // mail
     private val _email = MutableLiveData<String>("")
@@ -16,14 +56,18 @@ class RegisterViewModel : ViewModel() {
     fun setEmail (email : String){
         _email.value = email
     }
-
-
+    private fun resetEmail(){
+        _email.value = ""
+    }
 
     // password
     private val _password = MutableLiveData<String>("")
     val password: LiveData<String> get() = _password
     fun setPassword (pass : String){
         _password.value = pass
+    }
+    private fun resetPass(){
+        _password.value = ""
     }
 
     // password2
@@ -32,8 +76,33 @@ class RegisterViewModel : ViewModel() {
     fun setPassword2 (pass : String){
         _password2.value = pass
     }
+    private fun resetPass2(){
+        _password2.value = ""
+    }
 
 
+    // contador salidaRegistro/nuevoRegistro
+    // 1 - Fin de Registro
+    // 0 - Registro en marcha
+    private val _registro = MutableLiveData(0)
+    val registro: LiveData<Int> get() = _registro
+    fun setEstadoRegistro (estadoRegistro : Int){
+        _registro.value = estadoRegistro
+    }
+    private fun resetEstadoRegistro(){
+        _registro.value = 0
+    }
 
+
+    fun resetValues(){
+        resetNom()
+        resetEdat()
+        resetSexe()
+        resetCiutat()
+        resetEmail()
+        resetPass()
+        resetPass2()
+        resetEstadoRegistro()
+    }
 
 }
